9983cd292acd1e9ae294e1bd89d4f219f94bd9a3
Ayush_Shukla <ayush_shukla@Ninja-ITs-MacBook-Air.local>
2024-05-11 18:06:02 +0530

    Initial commit

---
+++ src/main/java/com/jobApp/JobApp/Job/Job.java
@@ -0,0 +1,69 @@
+package com.jobApp.JobApp.Job;
+
+public class Job {
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getMinSalary() {
+        return minSalary;
+    }
+
+    public void setMinSalary(String minSalary) {
+        this.minSalary = minSalary;
+    }
+
+    public String getMaxSalary() {
+        return maxSalary;
+    }
+
+    public void setMaxSalary(String maxSalary) {
+        this.maxSalary = maxSalary;
+    }
+
+    public String getLocation() {
+        return Location;
+    }
+
+    public void setLocation(String location) {
+        Location = location;
+    }
+
+    public Job(Long id, String title, String description, String minSalary, String maxSalary, String location) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.minSalary = minSalary;
+        this.maxSalary = maxSalary;
+        Location = location;
+    }
+
+    private String title;
+    private String description;
+    private String minSalary;
+    private String maxSalary;
+    private String Location;
+
+}
