4271460367bec3df751696e1f943da36c40bff81
Ayush_Shukla <ashukla4141@gmail.com>
2024-05-16 00:03:44 +0530

    Company crud APIs created

---
+++ src/main/java/com/jobApp/JobApp/company/CompanyService.java
@@ -0,0 +1,12 @@
+package com.jobApp.JobApp.company;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+public interface CompanyService {
+    List<Company> getAllCompanies();
+    boolean updateCompany(Company company, Long id);
+
+    void createCompany(Company company);
+}
